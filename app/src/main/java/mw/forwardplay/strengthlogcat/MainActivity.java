e1bcdde755aeae81ecd68d5cdf007a1b1f5cacec
Andrew Mfune <andiemfune@gmail.com>
2019-03-02 11:30:34 +0800

    Initial implementation of app with following features working:
    - UI implementation
    - Adding new sets and reps

---
+++ app/src/main/java/mw/forwardplay/strengthlogcat/MainActivity.java
@@ -0,0 +1,58 @@
+package mw.forwardplay.strengthlogcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String exerciseName;
+    private int setNumber;
+    private int repNumber;
+    private EditText exerciseEditText;
+    private TextView repCountTextView;
+    private TextView setCountTextView;
+    private LogSets setLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAdd(View view)
+    {
+        exerciseEditText = (EditText) findViewById(R.id.exercise_name);
+        repCountTextView = (TextView) findViewById(R.id.rep_count);
+        setCountTextView = (TextView) findViewById(R.id.set_count);
+        exerciseName = exerciseEditText.getText().toString();
+
+        if (!exerciseName.isEmpty())
+        {
+            setLog = SetLoggerFactory.getSetLogInstance(exerciseName);
+            switch (view.getId()){
+                case R.id.add_rep:
+                    if (setNumber == 0){
+                        setLog.addSet();
+                    }
+                    setLog.addRep();
+                    break;
+                case R.id.add_set:
+                    setLog.addSet();
+                    break;
+                default:
+                    break;
+            }
+            setNumber = setLog.getSetNumber();
+            repNumber = setLog.getRepNumber();
+            SetLoggerFactory.updateSetLog(exerciseName, setLog);
+            repCountTextView.setText(String.valueOf(repNumber));
+            setCountTextView.setText(String.valueOf(setNumber));
+        }else{
+            Toast.makeText(getApplicationContext(), "Exercise name is required!",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/mw/forwardplay/strengthlogcat/SetLoggerFactory.java
@@ -0,0 +1,109 @@
+package mw.forwardplay.strengthlogcat;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SetLoggerFactory {
+    private SetLoggerFactory(){}
+    private static Map<String, LogSets> exercises = new HashMap<>();
+
+    public static void updateSetLog(String exercise, LogSets logSets)
+    {
+        exercises.put(exercise, logSets);
+    }
+
+    public static LogSets getSetLogInstance(String exercise)
+    {
+        if(exercises.containsKey(exercise))
+        {
+            return exercises.get(exercise);
+        }else{
+            exercises.put(exercise, new LogSets());
+            return exercises.get(exercise);
+        }
+    }
+}
+
+class LogSets{
+    private List<Integer> setList = new ArrayList<>();
+    private int setNumber;
+    private int repNumber;
+
+    public void addRep()
+    {
+        repNumber +=1 ;
+    }
+
+    public void addSet()
+    {
+
+        setNumber += 1;
+        repNumber = 0;
+        addSetToSetList();
+    }
+
+    public void removeSet()
+    {
+        int difference = setNumber - 1;
+        if(setNumber >= 0)
+        {
+            setNumber = difference;
+            repNumber = setList.get(difference);
+        }else{
+            setNumber = 0;
+            repNumber = 0;
+        }
+    }
+
+    public void removeRep()
+    {
+        int difference = repNumber - 1;
+        if(repNumber >= 0)
+        {
+            repNumber = difference;
+        }else{
+            repNumber = 0;
+        }
+    }
+
+    private void addSetToSetList()
+    {
+        int setNumber = this.setNumber;
+        if(this.setNumber >=1)
+        {
+            setNumber -= 1;
+        }
+        setList.add(setNumber, repNumber);
+    }
+
+    public int getRepNumber() {
+        return repNumber;
+    }
+
+    public int getSetNumber() {
+        return setNumber;
+    }
+
+    public String toString()
+    {
+        StringBuilder repsStr = new StringBuilder();
+        int counter = 0;
+        int totalReps = 0;
+        for (int reps: setList)
+        {
+            totalReps += reps;
+            if(counter < setList.size())
+            {
+                repsStr.append(reps).append(" + ");
+            }else{
+                repsStr.append("=").append(totalReps);
+            }
+            ++counter;
+        }
+        return repsStr.toString();
+    }
+}
