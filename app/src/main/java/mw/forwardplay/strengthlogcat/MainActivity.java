f551c0ca151d94dcab1ba9437ded84df0502cf03
Andrew Mfune <andiemfune@gmail.com>
2019-03-02 12:41:23 +0800

    Implemented method to handle new exercise button click events.
    This method resets the whole thing

--- app/src/main/java/mw/forwardplay/strengthlogcat/MainActivity.java
+++ app/src/main/java/mw/forwardplay/strengthlogcat/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private String exerciseName;
     private int setNumber;
     private int repNumber;
+    private Button newExerciseButton;
     private EditText exerciseEditText;
     private TextView repCountTextView;
     private TextView setCountTextView;
@@ -24,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
         exerciseEditText = (EditText) findViewById(R.id.exercise_name);
         repCountTextView = (TextView) findViewById(R.id.rep_count);
         setCountTextView = (TextView) findViewById(R.id.set_count);
+        newExerciseButton = (Button) findViewById(R.id.new_exercise);
+
+        newExerciseButton.setEnabled(false);
+    }
+
+    public void onClickNewExercise(View view)
+    {
+        SetLoggerFactory.updateSetLog(exerciseName, setLog);
+        setNumber = 0;
+        repNumber = 0;
+        exerciseName = "";
+        exerciseEditText.setText("");
+        repCountTextView.setText(String.valueOf(repNumber));
+        setCountTextView.setText(String.valueOf(setNumber));
+        exerciseEditText.setEnabled(true);
     }
 
     public void onClickSetAdjusters(View view)
@@ -32,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (!exerciseName.isEmpty())
         {
+            newExerciseButton.setEnabled(true);
             setLog = SetLoggerFactory.getSetLogInstance(exerciseName);
             switch (view.getId()){
                 case R.id.add_rep:
