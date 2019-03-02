f4f227f7af7c0571cfca45afdfe0c517295ee208
Andrew Mfune <andiemfune@gmail.com>
2019-03-02 17:17:12 +0800

    Added a custom toolbar with actions to create new exercise and share exercise log with apps that support plain text

--- app/src/main/java/mw/forwardplay/strengthlogcat/MainActivity.java
+++ app/src/main/java/mw/forwardplay/strengthlogcat/MainActivity.java
@@ -1,40 +1,47 @@
 package mw.forwardplay.strengthlogcat;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private String exerciseName;
     private int setNumber;
     private int repNumber;
-    private Button newExerciseButton;
     private EditText exerciseEditText;
     private TextView repCountTextView;
     private TextView setCountTextView;
     private TextView exerciseLogTextView;
     private LogSets setLog;
     private String exerciseLog;
+    private ShareActionProvider shareContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         exerciseEditText = (EditText) findViewById(R.id.exercise_name);
         repCountTextView = (TextView) findViewById(R.id.rep_count);
         setCountTextView = (TextView) findViewById(R.id.set_count);
-        newExerciseButton = (Button) findViewById(R.id.new_exercise);
         exerciseLogTextView = (TextView) findViewById(R.id.exercise_log);
-
-        newExerciseButton.setEnabled(false);
     }
 
-    public void onClickNewExercise(View view)
+    public void onClickNewExercise()
     {
         SetLoggerFactory.updateSetLog(exerciseName, setLog);
         setNumber = 0;
@@ -52,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
         if (!exerciseName.isEmpty())
         {
-            newExerciseButton.setEnabled(true);
             setLog = SetLoggerFactory.getSetLogInstance(exerciseName);
             switch (view.getId()){
                 case R.id.add_rep:
@@ -89,9 +95,46 @@ public class MainActivity extends AppCompatActivity {
             setCountTextView.setText(String.valueOf(setNumber));
             exerciseLog = SetLoggerFactory.getStrLog();
             exerciseLogTextView.setText(exerciseLog);
+            setShareContentActionProvider();
         }else{
             Toast.makeText(getApplicationContext(), "Exercise name is required!",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_toolbar_items, menu);
+        MenuItem shareItem = menu.findItem(R.id.share_info);
+        shareContent = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        setShareContentActionProvider();
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        switch (menuItem.getItemId()){
+            case R.id.next_exercise:
+                onClickNewExercise();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    void setShareContentActionProvider()
+    {
+        StringBuilder subject = new StringBuilder();
+        Date currentDate = new Date();
+        subject.append("Strength Training: ").append(currentDate);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject.toString());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, exerciseLog);
+        shareContent.setShareIntent(shareIntent);
     }
 }
